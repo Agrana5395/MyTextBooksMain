d3d8bea2bdb49c3be80fe4f7a457b9e29eaa594c
Alfredo Granado <alfred_grand@outlook.com>
2017-03-22 01:05:01 -0400

    Add project files.

---
+++ MyTextBooksMain/Models/Books.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace MyTextBooksMain.Models
+{
+    public class Books
+    {
+        public int id { get; set; }
+        [Required]
+        [Display(Name = "Book Name" )]
+        public string BookName { get; set; }
+
+       [Display(Name = "Books description")]
+       
+        public string BookDescription { get; set; }
+
+        [Display(Name = "How many Books with the same Name")]
+        [Range(1,99)]
+        [Required]
+        public int BookQuantity { get; set; }
+        [Display(Name = "Book Price" )]
+        [Range(0,100000000)]
+        [Required]
+        public double BookPrice { get; set; }
+        [Display(Name = "Book Quality ")]
+        [Required]
+        public string Ratings { get; set; }
+       
+                
+    }
+}
---
+++ MyTextBooksMain/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyTextBooksMain.Startup))]
+namespace MyTextBooksMain
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
